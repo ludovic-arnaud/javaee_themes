32e1a2616d4bc79d7e7de9a1b93f564e2b55f65f
Ludovic Arnaud <ludovic.arnaud@live.fr>
2020-06-10 14:50:15 +0200

    ajout du script sql pour le jeu de données

---
+++ src/main/resources/script/alimentation.sql
@@ -0,0 +1,14 @@
+INSERT INTO utilisateur(typediscriminant, codepersonnelcentre, email, nom, password, prenom)
+	VALUES ('ANIMATEUR', 'AFPAANIM01', 'mathilde.dubois@afpa.fr', 'Dubois', 'passwordmathilde', 'Mathilde');
+	
+INSERT INTO utilisateur(typediscriminant, codepersonnelcentre, email, nom, password, prenom)
+	VALUES ('ANIMATEUR', 'AFPAANIM02', 'pierre.martin@afpa.fr', 'Martin', 'passwordpierre', 'Pierre');	
+	
+INSERT INTO utilisateur(typediscriminant, codepersonnelcentre, email, nom, password, prenom)
+	VALUES ('ANIMATEUR', 'AFPAANIM03', 'aline.dupont@afpa.fr', 'Dupont', 'passwordaline', 'Aline');
+	
+INSERT INTO theme(couleur, description, descriptiondetaillee, nom)
+	VALUES ('Jaune', 'Contient les ateliers Job Cible', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus congue, turpis eu gravida accumsan, eros leo cursus neque, eget suscipit ipsum tellus nec ante. Donec diam magna, auctor eu metus nunc.', 'Job Cible');
+	
+INSERT INTO theme(couleur, description, descriptiondetaillee, nom)
+	VALUES ('Rouge', 'Contient les ateliers Soft Skills', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus congue, turpis eu gravida accumsan, eros leo cursus neque, eget suscipit ipsum tellus nec ante. Donec diam magna, auctor eu metus nunc.', 'Soft Skills');
