742b2ad3c36705eb58e384e9263034ca4622e9f2
Ludovic Arnaud <ludovic.arnaud@live.fr>
2020-06-15 12:17:45 +0200

    mise à jour du fichier d'alim de la bdd

--- src/main/resources/script/alimentation.sql
+++ src/main/resources/script/alimentation.sql
@@ -8,7 +8,8 @@ INSERT INTO utilisateur(typediscriminant, codepersonnelcentre, email, nom, passw
 	VALUES ('ANIMATEUR', 'AFPAANIM03', 'aline.dupont@afpa.fr', 'Dupont', 'passwordaline', 'Aline');
 	
 INSERT INTO theme(couleur, description, descriptiondetaillee, nom)
-	VALUES ('Jaune', 'Contient les ateliers Job Cible', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus congue, turpis eu gravida accumsan, eros leo cursus neque, eget suscipit ipsum tellus nec ante. Donec diam magna, auctor eu metus nunc.', 'Job Cible');
+	VALUES ('#FFFF00', 'Contient les ateliers Job Cible', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus congue, turpis eu gravida accumsan, eros leo cursus neque, eget suscipit ipsum tellus nec ante. Donec diam magna, auctor eu metus nunc.', 'Job Cible');
 	
 INSERT INTO theme(couleur, description, descriptiondetaillee, nom)
-	VALUES ('Rouge', 'Contient les ateliers Soft Skills', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus congue, turpis eu gravida accumsan, eros leo cursus neque, eget suscipit ipsum tellus nec ante. Donec diam magna, auctor eu metus nunc.', 'Soft Skills');
+	VALUES ('#FF0000', 'Contient les041150
+ ateliers Soft Skills', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus congue, turpis eu gravida accumsan, eros leo cursus neque, eget suscipit ipsum tellus nec ante. Donec diam magna, auctor eu metus nunc.', 'Soft Skills');
