05192f80bba79a9477ac6ba1f4a89adb3835711d
Ludovic Arnaud <ludovic.arnaud@live.fr>
2020-06-19 13:47:48 +0200

    maj du fichier d'alimentation de la base de données

--- src/main/resources/script/alimentation.sql
+++ src/main/resources/script/alimentation.sql
@@ -13,3 +13,11 @@ INSERT INTO theme(couleur, description, descriptiondetaillee, nom)
 INSERT INTO theme(couleur, description, descriptiondetaillee, nom)
 	VALUES ('#FF0000', 'Contient les041150
  ateliers Soft Skills', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus congue, turpis eu gravida accumsan, eros leo cursus neque, eget suscipit ipsum tellus nec ante. Donec diam magna, auctor eu metus nunc.', 'Soft Skills');
+ 
+ INSERT INTO activite(
+	description, descriptiondetaillee, duree, nom)
+	VALUES ('Comment créer un CV', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus congue, turpis eu gravida accumsan, eros leo cursus neque, eget suscipit ipsum tellus nec ante.', '3', 'CV');
+	
+	INSERT INTO activite(
+	description, descriptiondetaillee, duree, nom)
+	VALUES ('Comment réussir son entretien', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus congue, turpis eu gravida accumsan, eros leo cursus neque, eget suscipit ipsum tellus nec ante', '4', 'Entretiens');
